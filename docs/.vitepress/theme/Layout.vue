abf66bd34a2e3842fe8715d88d415eae6aa79d34
GDa Rty <kuzhe@MacBook-Air.local>
2025-12-26 10:14:36 +0500

    feat: add Yandex.Metrika noscript fallback and head script

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,3 +1,19 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <slot />
+    <!-- noscript fallback for Yandex.Metrika -->
+    <noscript>
+      <div>
+        <img src="https://mc.yandex.ru/watch/106038322" style="position:absolute; left:-9999px;" alt="" />
+      </div>
+    </noscript>
+  </DefaultTheme.Layout>
+</template>
+
 // layout removed to reset to default theme
 
 
